8a3814f1f78589fef4349d60dc697ebb33c1d700
shalev5252 <shalevshasha@gmail.com>
2026-01-12 10:33:42 +0200

    refactor(server): rename status constants to match camelCase convention

--- server/src/services/todoService.js
+++ server/src/services/todoService.js
@@ -1,6 +1,7 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { taskStatusOptions as statuses } from '../constants/status.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
